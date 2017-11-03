0426dd0930833047154eb8bf773607ea34166282
Bobby Reynolds <bobby@reynoldsbd.net>
2017-11-02 21:37:25 -0700

    Update config.
    Updating after a week of debugging.

--- config.sh
+++ config.sh
@@ -16,22 +16,57 @@
 # hardware configuration
 WAN_IF=em0
 LAN_IF=em1
-BAK_IF=em3 # TODO: not sure what this will be until after installed
+
+
+echo "Configuring doas"
+cp ./etc/doas.conf /etc/doas.conf
 
 
 echo "Configuring sysctl"
+# Configures sysctl toggles necessary to allow IP traffic to be routed
+# through the system. Also enables GRE packets, which are necessary
+# for VPN traffic.
 cp ./etc/sysctl.conf /etc/sysctl.conf
-cat /etc/sysctl.conf | xargs -L 1 sysctl
+cat /etc/sysctl | xargs sysctl
+
+
+echo "Configuring PF rules"
+# Configures the PF firewall rules for this router. This includes NAT
+# for IPv4 traffic and port forwarding for all internal services
+cp ./etc/pf.conf /etc/pf.conf
+pfctl -ef /etc/pf.conf
 
 
 echo "Configuring network interfaces"
+# Sets up configuration for network interfaces, both internal and
+# external
+cp ./etc/myname /etc/myname
+rm -f /etc/mygate
 cp ./etc/hostname.wan /etc/hostname.$WAN_IF
 cp ./etc/hostname.lan /etc/hostname.$LAN_IF
-cp ./etc/hostname.bak /etc/hostname.$BAK_IF
 /bin/sh /etc/netstart
 
 
+echo "Configuring DHCPv6 Client"
+# Comcast doesn't support full IPv6 autoconfiguration, so we have
+# to use separate utility to configure IPv6 for this host and the
+# internal network
+pkg_add dhcpcd
+cp ./etc/dhcpcd.conf /etc/dhcpcd.conf
+rcctl enable dhcpcd
+rcctl start dhcpcd
+
+
+echo "Configuring rtadvd"
+# This enables true IPv6 autoconfiguration within the LAN.
+rcctl enable rtadvd
+rcctl set rtadvd flags $LAN_IF
+rcctl start rtadvd
+
+
 echo "Configuring unbound"
+# This gives us a caching DNS resolver for the local network. In my
+# case, this improves most DNS query time from >50ms to ~5ms.
 cp ./var/etc/unbount/etc/unbound.conf /var/unbound/etc/unbound.conf
 rcctl enable unbound
 rcctl start unbound
@@ -40,14 +75,9 @@ rcctl start unbound
 echo "Configuring dhcpd"
 pkg_add dhcpd
 cp ./etc/dhcpd.conf /etc/dhcpd.conf
-rcctl set dhcpd flags $LAN_IF $BAK_IF
 rcctl enable dhcpd
+rcctl set dhcpd flags $LAN_IF
 rcctl start dhcpd
-
-
-echo "Configuring and enabling PF"
-cp ./etc/pf.conf /etc/pf.conf
-pfctl -ef /etc/pf.conf
 
 
 echo "Installation complete."
